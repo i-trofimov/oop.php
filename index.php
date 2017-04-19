3c4c9e56f126d751d1671986e4ddee0f7d4f3c13
imtrofimov <im.trofimov@yandex.com>
2017-04-20 00:33:31 +0300

    #3
    Methods of classes and function

--- index.php
+++ index.php
@@ -8,23 +8,21 @@
 		<?php
 			class User {
 				public $name = 'Имя';
+				//public $surname = 'Фамилия';
 				public $password = 'Пароль';
 				public $email = 'Eмейл';
 				public $city = 'Город';
+				
+				function getInfo()
+				{
+					return $this->name . ' ' . $this->surname;
+				}
 			}
 			
-			$admin = new User();
-			echo $admin->name;
-			
-			echo '<br/><br/>';
-			
-			$user1 = new User();
-			$user1->name = 'Вася';
-			$user1->password = 'qwerty';
-			$user1->email = 'vasya@admin.ru';
-			$user1->city = 'Moscow';
-			$user1->surname = 'Ivanov';
-			echo $user1->name . ', ' . $user1->surname . ', ' . $user1->password . ', ' . $user1->email . ', ' . $user1->city;
+			$class = new User();
+			$class->name = 'Alexey ';
+			$class->surname = 'Ivanov';
+			echo $class->getInfo();
 		?>
 	</body>
 </html>
