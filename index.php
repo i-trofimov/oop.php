336b1840a98188c571b49b4e8aa36ebf9fab245c
imtrofimov <im.trofimov@yandex.com>
2017-04-21 21:34:41 +0300

    #12
    set, get, clone

--- index.php
+++ index.php
@@ -6,30 +6,52 @@
 	</head>
 	<body>
 		<?php
-			trait Hello
+			//class User
+			//{
+			//	private $name;
+			//	private $city;
+			//	private $id;
+			//	
+			//	public function __construct($name, $city)
+			//	{
+			//		$this->name = $name;
+			//		$this->city = $city;
+			//	}
+			//	
+			//	public function setId($id)
+			//	{
+			//		$this->id = $id;
+			//	}
+			//	
+			//	public function __clone()
+			//	{
+			//		$this->id = 0;
+			//	}
+			//}
+			//
+			//$user1 = new User('Alexey', 'Kiev');
+			//$user1->setId(5662);
+			
+			//$user2 = clone $user1;
+			
+			class GetSet 
 			{
-				public function sayHello()
+				private $number = 1;
+				
+				public function __get($name)
 				{
-					echo 'Hello';
+					echo "You get {$name}";
+				}
+				
+				public function __set($name, $val)
+				{
+					echo "You set {$name} to ";
 				}
 			}
 			
-			trait World
-			{
-				public function sayWorld()
-				{
-					echo 'World';
-				}
-			}
-			
-			class myHelloWorld
-			{
-				use Hello, World;
-			}
-			
-			$obj = new myHelloWorld();
-			$obj->sayHello();
-			$obj->sayWorld();
+			$obj = new GetSet();
+			echo $obj->number;
+			echo $obj->number = 689;
 		?>
 	</body>
 </html>
